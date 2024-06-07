9fb735893a1e32bffe17924ec5fcb93b6d0714d4
TiniT <letter.tinit@gmail.com>
2024-06-07 08:53:00 +0700

    publish local

---
+++ VideoPlayer/App/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Treasure Letter on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
